8a6566ca9aefe66654ad3858db192714bac96c7c
John Nguyen <john.nguyen@eon.com>
2025-01-05 23:13:41 +0100

    Add gitlab CI with pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Lib
+
+A monorepo for all AWS related SDK's, CDK's, SAM Templates, and AWS related tools.
--- cloudcatalog/astro.config.ts
+++ cloudcatalog/astro.config.ts
@@ -4,6 +4,8 @@ import starlightTypeDoc, { typeDocSidebarGroup } from "starlight-typedoc";
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: "public",
+  publicDir: "assets",
   integrations: [
     starlight({
       title: "HEMS AWS-lib",
